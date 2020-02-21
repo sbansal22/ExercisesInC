83bb3a61ca612c557446e15d644815ee19048b6a
Sparsh Bansal <42981009+sbansal22@users.noreply.github.com>
2020-02-20 19:06:25 -0500

    Add files via upload

---
+++ exercises/ex03/tee.c
@@ -0,0 +1,66 @@
+/*
+Author: Sparsh Bansal
+Software Systems SP 2020, Homework Exercise 3
+
+A program that implements the function <Tee>
+
+References: Head First C Book
+            CA, Prava
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    char ch;
+    char filepath[100];
+    char text[100000];
+    int append = 0;
+    int ignore = 0;
+    int diagnose = 0;
+    int default = 0;
+    int count = 0;
+    FILE *edit;
+    
+    while ((ch = getopt(argc, argv, "aip")) != EOF)
+        switch(ch) {
+            case 'a':
+                append = 1;
+            break;
+
+            case 'i':
+                ignore = 1;
+            break;
+
+            case 'p':
+                diagnose = 1;
+            break;
+
+            default: 
+                fprintf(stderr, "Unknown option: '%s'\n", optarg);
+                return 1;
+        }
+    
+    argc -= optind;
+    argv += optind;
+
+    while (fgets(text, sizeof(text), stdin) != NULL) {
+        for (count = 0; count < argc; count++) {
+            if (append) {
+            edit = fopen(filepath,"a");
+            fprintf(edit, "%s\n",text);
+            fclose(edit);
+            }
+            if (ignore) {
+
+            }
+            if (diagnose) {
+                
+            }
+        }
+    }
+    return 0;
+}
