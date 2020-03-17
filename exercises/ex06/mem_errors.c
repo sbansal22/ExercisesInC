d4516ee014ed8b91ec83b4732c3368078b8c825a
Sparsh Bansal <42981009+sbansal22@users.noreply.github.com>
2020-03-17 14:27:42 +0530

    Submitting Homework 6

--- exercises/ex06/mem_errors.c
+++ exercises/ex06/mem_errors.c
@@ -29,16 +29,20 @@ int main()
     int *array2 = malloc(100 * sizeof (int));
 
     // valgrind does not bounds-check static arrays
-    read_element(array1, -1);
-    read_element(array1, 100);
+    // read_element(array1, -1);
+    // read_element(array1, 100);
 
+    read_element(array1, 0);
+    read_element(array1, 99);
     // but it does bounds-check dynamic arrays
-    read_element(array2, -1);
-    read_element(array2, 100);
+    // read_element(array2, -1);
+    // read_element(array2, 100);
 
+    read_element(array2, 0);
+    read_element(array2, 99);
     // and it catches use after free
     free(use_after_free);
-    *use_after_free = 17;
+    // *use_after_free = 17;
 
     // never_free is definitely lost
     *never_free = 17;
@@ -46,11 +50,13 @@ int main()
     // the following line would generate a warning
     // free(&never_allocated);
 
-    // but this one doesn't
-    free_anything(&never_allocated);
+    // but this one doesn'tread_element(array1, 0);
+    // free_anything(&never_allocated);
 
     free(free_twice);
-    free(free_twice);
+    // free(free_twice);
 
+    free(never_free);
+    free(array2);
     return 0;
 }
