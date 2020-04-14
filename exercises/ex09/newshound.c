9250a0e7ab2ec3a7287fb771c88b98a1f158c8a9
Sparsh Bansal <42981009+sbansal22@users.noreply.github.com>
2020-04-14 07:26:10 -0400

    Submitting Homework 9

--- exercises/ex09/newshound.c
+++ exercises/ex09/newshound.c
@@ -5,19 +5,41 @@ Downloaded from https://github.com/twcamper/head-first-c
 Modified by Allen Downey.
 */
 
+/*
+    This program only prints articles from the first RSS feed
+    because it calls the execle() function, which replaces the
+    the current function by running a new program. Any exec()
+    function would do this termination.  
+
+    # Reference - Exercises in C, fork.c  
+*/ 
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <wait.h>
 
+// errno is an external global variable that contains
+// error information
+extern int errno;
 
 void error(char *msg)
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(1);
+}
+
+// get_seconds returns the number of seconds since the
+// beginning of the day, with microsecond precision
+double get_seconds() {
+    struct timeval tv[1];
+
+    gettimeofday(tv, NULL);
+    return tv->tv_sec + tv->tv_usec / 1e6;
 }
 
 int main(int argc, char *argv[])
@@ -38,15 +60,53 @@ int main(int argc, char *argv[])
     int num_feeds = 5;
     char *search_phrase = argv[1];
     char var[255];
+    int status;
+    pid_t pid;
+    double start, stop;
+
+    // get the start time
+    start = get_seconds();
 
     for (int i=0; i<num_feeds; i++) {
         sprintf(var, "RSS_FEED=%s", feeds[i]);
         char *vars[] = {var, NULL};
 
-        int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
-        if (res == -1) {
-            error("Can't run script.");
+        pid = fork();
+
+        /* check for an error */
+        if (pid == -1) {
+            fprintf(stderr, "fork failed: %s\n", strerror(errno));
+            perror(argv[0]);
+            exit(1);
+        }
+
+        /* see if we're the parent or the child */
+        if (pid == 0) {
+            int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
+            if (res == -1) {
+                error("Can't run script.");
+            }
+            exit(i);
         }
     }
-    return 0;
+
+for (int i=0; i<num_feeds; i++) {
+        pid = wait(&status);
+
+        if (pid == -1) {
+            fprintf(stderr, "wait failed: %s\n", strerror(errno));
+            perror(argv[0]);
+            exit(1);
+        }
+
+        // check the exit status of the child
+        status = WEXITSTATUS(status);
+        if(status != 0){
+            printf("Child %d exited with error code %d.\n", pid, status);
+        }
+    }
+    // compute the elapsed time
+    stop = get_seconds();
+    printf("Elapsed time = %f seconds.\n", stop - start);
+    exit(0);
 }
